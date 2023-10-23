3e9aae55474aea856a67e0988e44d6bf8f9ff8bf
Your Name <you@example.com>
2023-10-23 21:57:52 +0300

    new task

--- cypress/e2e/orangeHRM.cy.ts
+++ cypress/e2e/orangeHRM.cy.ts
@@ -9,30 +9,4 @@ describe("senario #1", () => {
     cy.visit("/auth/login");
     loginAsAdmin.loginAdmin();
   });
-
-  it("Create Employee Then Login with the new Employee", () => {
-    const username = faker.internet.userName();
-    cy.fixture("employeeData").as("empData");
-    cy.get("@empData").then((data: any) => {
-      createEmployeeViaApi.addEmployeeViaApi(
-        data.empID,
-        data.firstName,
-        data.lastName,
-        data.middleName,
-        username,
-        data.password,
-      );
-      loginWithEmployeeViaUi.loginViaUi(username, data.password);
-    });
-  });
-
-  it("create a candidate Then upload file and make the assertion", () => {
-    cy.visit("/recruitment/addCandidate");
-    addCandidate.createNewCandidate(
-      faker.person.firstName(),
-      faker.person.middleName(),
-      faker.person.lastName(),
-    );
-    addCandidate.uploadFileWithAssertion("cypress/fixtures/RJOUB123.pdf");
-  });
 });
