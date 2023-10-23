aab51f996e2f8fb2ec745cc7b53cbc1ddf81b92e
Your Name <you@example.com>
2023-10-23 22:13:56 +0300

    ok

--- cypress/e2e/orangeHRM.cy.ts
+++ cypress/e2e/orangeHRM.cy.ts
@@ -4,6 +4,12 @@ import { createEmployeeViaApi } from "../support/helpers/createEmployeeViaApi";
 import { loginWithEmployeeViaUi } from "../support/helpers/loginWithEmployeeViaUi";
 import { addCandidate } from "../support/helpers/addCandidate";
 
+/*
+1- we need to add employee by beeing loggen in with admin
+2-we need to give that added employee number of entitlement
+3-
+*/
+
 describe("senario #1", () => {
   beforeEach(() => {
     cy.visit("/auth/login");
